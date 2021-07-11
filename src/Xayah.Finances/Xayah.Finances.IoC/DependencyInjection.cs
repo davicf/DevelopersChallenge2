91f889645123a19178359ad4d0f61fdd59f5afd9
Davi Carvalho <davi.carvalho@ed.company>
2021-07-11 18:33:36 -0300

    feat: add container IoC

---
+++ src/Xayah.Finances/Xayah.Finances.IoC/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xayah.Finances.Business.Services;
+using Xayah.Finances.Contracts.Business.Accounts;
+
+namespace Xayah.Finances.IoC
+{
+    public static class DependencyInjection
+    {
+        public static void AddMyDependencies(this IServiceCollection services)
+        {
+            #region Services
+            services.AddScoped<IAccountService, AccountService>();
+            //services.Scan(scan => scan.FromApplicationDependencies()
+            //                          .AddClasses(c => c.Where(x => x.Name.EndsWith("Service")))
+            //                          .AsImplementedInterfaces()
+            //                          .WithScopedLifetime());
+            #endregion
+        }
+    }
+}
